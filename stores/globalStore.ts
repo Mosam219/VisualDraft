2e60cbe828ccd137422e60c9f142a2052209ea43
personal <mosampatel78@gmail.com>
2023-12-18 22:09:50 +0530

    removed unnecessary import

--- stores/globalStore.ts
+++ stores/globalStore.ts
@@ -1,6 +1,4 @@
 import { atom } from 'jotai';
-import { MODES } from '@/app/project/[projectId]/__components/ToolBar/constants';
-import { Drawable } from 'roughjs/bin/core';
 import { GlobalStoreType } from './types';
 
 export const INITIAL_STATE: GlobalStoreType = {
